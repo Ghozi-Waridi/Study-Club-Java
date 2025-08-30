61bb263fb5c7ad0ec469994bb9d54ba85b7b542c
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-08-30 21:04:07 +0700

    :star: ADD: Menambahkan materi Day-2
    Menambahkan Materi hari ke 2 dengan materi Pengkondisin (expression,
    statement, block, if else, switch case, ternary operator)

---
+++ Day-2/StudyCase.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class StudyCase {
+  public static void main(String[] args) {
+    // Bikin obejct scanner untuk melakukan Inputan.
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan nilai siswa (0-100): ");
+    int nilai = input.nextInt();
+
+    // Melakukan Pengecekan dengan menggunakan ternary operator
+    String status = (nilai >= 70) ? "Lulus"
+        : (nilai >= 50) ? "agak lulus" : (nilai == 0) ? "Tidak lulussss" : "Drout OUt";
+    System.out.println("Status: " + status);
+
+    char kategori;
+    if (nilai >= 85) {
+      kategori = 'A';
+    } else if (nilai != 70) {
+      kategori = 'B';
+    } else if (nilai > 55) {
+      kategori = 'C';
+    } else {
+      kategori = 'D';
+    }
+
+    System.out.println("Keterangan: ");
+    switch (kategori) {
+      case 'A':
+        System.out.println("Sangat Baik, pertahankan!");
+        break;
+      case 'B':
+        System.out.println("Baik, tingkatkan lagi belajarmu.");
+        break;
+      case 'C':
+        System.out.println("Cukup, perlu belajar lebih giat.");
+        break;
+      case 'D':
+        System.out.println("Kurang, jangan menyerah dan coba lagi!");
+        break;
+    }
+
+    if (kategori == 'A') {
+
+      System.out.println("Sangat Baik, pertahankan!");
+
+    } else if (kategori == 'B') {
+
+      System.out.println("Baik, tingkatkan lagi belajarmu.");
+
+    } else if (kategori == 'C') {
+      System.out.println("Cukup, perlu belajar lebih giat.");
+    } else if (kategori == 'D') {
+      System.out.println("Kurang, jangan menyerah dan coba lagi!");
+    }
+
+    input.close();
+  }
+}
---
+++ Day-2/pengkondisian.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class pengkondisian {
+  public static void main(String[] args) {
+
+    // int angka = 10;
+    // angka++;
+    // System.out.println(angka);
+
+    int time = 20;
+
+    if (time < 18) {
+      System.out.println("Good Day");
+      System.out.println("Kondisi Benar");
+    } else {
+      System.out.println("Good Evening");
+      System.out.println("Kondisi salah");
+    }
+    System.out.println("Selesai..");
+
+    System.out.println("=======================");
+    /**
+     * 
+     */
+    int nilai = 80;
+    switch (nilai) {
+      case 80:
+        System.out.println("B");
+        int angka_2 = 1 + 2;
+        nilai++;
+        System.out.println("Case 80");
+        break;
+      case 70:
+        System.out.println("C");
+        break;
+      default:
+        System.out.println("Angka tidak sesuai");
+    }
+    System.out.println("Switch Case Selesai");
+
+    System.out.println("=======================");
+
+    int umur = 100;
+    String orang = (umur == 100) ? "tua" : "dewasa";
+
+    // if (umur == 100) {
+    // orang = "tua";
+    // } else {
+    // orang = "dewasa";
+    // }
+
+    System.out.println(orang);
+
+    /**
+     * Input Keyboard
+     * Scanner => Salah satu tipe data non primitif yaitu
+     * Tipe data Object (tipe data buatan)
+     * 
+     */
+
+    System.out.println("=======================");
+
+    // Membuat sebuah variabel dengan tipe dara Scanner (non primitif)
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter Username : ");
+
+    // Membuat Sebuah Inputan.
+    String userName = input.nextLine();
+    System.out.println("Selamat Datang " + userName + " Di Teknik Informatika UIN Malang ");
+
+  }
+}
